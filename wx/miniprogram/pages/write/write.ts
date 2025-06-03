ae032b9e0e3ac1b13dc5b8517146bdde9ba83503
csfwff <csfwff@163.com>
2025-06-03 20:09:58 +0800

    add write

--- wx/miniprogram/pages/write/write.ts
+++ wx/miniprogram/pages/write/write.ts
@@ -5,14 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    minWriteHeight:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    const windowInfo = wx.getWindowInfo()
+    console.log(windowInfo)
+    this.setData({
+      minWriteHeight:windowInfo.windowHeight
+    })
   },
 
   /**
